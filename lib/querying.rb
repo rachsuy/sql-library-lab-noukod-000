8b2b3777e81ec342ed0573694ca303765a14d6eb
Rachele Suy <rachelesuy@gmail.com>
2019-06-05 14:40:31 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,8 +35,6 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
   "SELECT characters.name FROM characters
-   JOIN books ON books.id = character_books.book_id
-   JOIN character_books ON characters.id = character_books.character_id
-; "
+   JOIN books ON books.id = character_books.book_id; "
 
 end
