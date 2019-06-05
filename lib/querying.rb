6bd83bc152d25335048e86aa176a14d71e56efe9
Rachele Suy <rachelesuy@gmail.com>
2019-06-05 14:25:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,6 +25,11 @@ end
 
 def select_series_title_with_most_human_characters
   "Write your SQL query here"
+  "SELECT title FROM series 
+   JOIN characters ON series.id = characters.series_id
+   WHERE characters.species = 'human'
+   GROUP BY series.title
+   LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
