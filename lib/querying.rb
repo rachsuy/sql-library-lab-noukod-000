1696e142fddae10fd1691c38ef34a3f8d47b9575
Rachele Suy <rachelesuy@gmail.com>
2019-06-05 14:02:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,8 +17,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "Write your SQL query here"
   "SELECT authors.name FROM authors
-   JOIN series ON subgenres.id = series.subgenre_id
-   JOIN authors ON authors.id = series.author_id; "
+   INNER JOIN series ON subgenres.id = series.subgenre_id
+   INNER JOIN authors ON authors.id = series.author_id; "
 end
 
 def select_series_title_with_most_human_characters
