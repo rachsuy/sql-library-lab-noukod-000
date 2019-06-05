178cf60a18dcf38cf6473d0ce4d93b8a3db5d82d
Rachele Suy <rachelesuy@gmail.com>
2019-06-05 13:47:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,6 +16,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "Write your SQL query here"
+  "SELECT name, subgenre_id FROM subgenres JOIN series"
 end
 
 def select_series_title_with_most_human_characters
